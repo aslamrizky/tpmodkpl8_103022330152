064c24c121e839fafb35151a9fbf0153b2430a29
Aslam Rizky <aslamrizky81@gmail.com>
2025-04-12 11:31:13 +0700

    menyelesaikan program dengan runtime configuration

---
+++ CovidConfig.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace tpmod8
+{
+    class CovidConfig
+    {
+        private string satuanSuhu;
+        private int batasHariDemam;
+        private string pesanDitolak;
+        private string pesanDiterima;
+        private string configFilePath = "covid_config.json";
+
+        public CovidConfig()
+        {
+            this.satuanSuhu = "celcius";
+            this.batasHariDemam = 14;
+            this.pesanDitolak = "Anda tidak diperbolehkan masuk ke dalam gedung ini";
+            this.pesanDiterima = "Anda dipersilahkan untuk masuk ke dalam gedung ini";
+
+            try
+            {
+                LoadConfig();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Config file not found or invalid. Using default values.");
+                SaveConfig();
+            }
+        }
+
+        public void LoadConfig()
+        {
+            if (File.Exists(configFilePath))
+            {
+                string jsonString = File.ReadAllText(configFilePath);
+                JsonDocument doc = JsonDocument.Parse(jsonString);
+                JsonElement root = doc.RootElement;
+
+                this.satuanSuhu = root.GetProperty("satuan_suhu").GetString();
+                this.batasHariDemam = int.Parse(root.GetProperty("batas_hari_deman").GetString());
+                this.pesanDitolak = root.GetProperty("pesan_ditolak").GetString();
+                this.pesanDiterima = root.GetProperty("pesan_diterima").GetString();
+            }
+        }
+
+        public void SaveConfig()
+        {
+            var options = new JsonSerializerOptions
+            {
+                WriteIndented = true
+            };
+
+            var configObj = new
+            {
+                satuan_suhu = this.satuanSuhu,
+                batas_hari_deman = this.batasHariDemam.ToString(),
+                pesan_ditolak = this.pesanDitolak,
+                pesan_diterima = this.pesanDiterima
+            };
+
+            string jsonString = JsonSerializer.Serialize(configObj, options);
+            File.WriteAllText(configFilePath, jsonString);
+        }
+
+        public string GetSatuanSuhu()
+        {
+            return this.satuanSuhu;
+        }
+
+        public int GetBatasHariDemam()
+        {
+            return this.batasHariDemam;
+        }
+
+        public string GetPesanDitolak()
+        {
+            return this.pesanDitolak;
+        }
+
+        public string GetPesanDiterima()
+        {
+            return this.pesanDiterima;
+        }
+
+        public void SetSatuanSuhu(string satuanSuhu)
+        {
+            this.satuanSuhu = satuanSuhu;
+            SaveConfig();
+        }
+
+        public void SetBatasHariDemam(int batasHariDemam)
+        {
+            this.batasHariDemam = batasHariDemam;
+            SaveConfig();
+        }
+
+        public void SetPesanDitolak(string pesanDitolak)
+        {
+            this.pesanDitolak = pesanDitolak;
+            SaveConfig();
+        }
+
+        public void SetPesanDiterima(string pesanDiterima)
+        {
+            this.pesanDiterima = pesanDiterima;
+            SaveConfig();
+        }
+
+        public void UbahSatuan()
+        {
+            if (this.satuanSuhu.ToLower() == "celcius")
+            {
+                this.satuanSuhu = "fahrenheit";
+            }
+            else
+            {
+                this.satuanSuhu = "celcius";
+            }
+            SaveConfig();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,65 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello");
+﻿using System;
+using tpmod8;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        CovidConfig config = new CovidConfig();
+
+        Console.WriteLine("Current Configuration:");
+        Console.WriteLine("Satuan Suhu: " + config.GetSatuanSuhu());
+        Console.WriteLine("Batas Hari Demam: " + config.GetBatasHariDemam());
+        Console.WriteLine();
+
+        Console.WriteLine("Berapa suhu badan anda saat ini? Dalam nilai " + config.GetSatuanSuhu());
+        double suhu = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Berapa hari yang lalu (perkiraan) anda terakhir memiliki gejala deman?");
+        int hariDemam = Convert.ToInt32(Console.ReadLine());
+
+        bool isTemperatureNormal = false;
+        bool isFeverDaysSafe = false;
+
+        if (config.GetSatuanSuhu().ToLower() == "celcius")
+        {
+            if (suhu >= 36.5 && suhu <= 37.5)
+            {
+                isTemperatureNormal = true;
+            }
+        }
+        else if (config.GetSatuanSuhu().ToLower() == "fahrenheit")
+        {
+            if (suhu >= 97.7 && suhu <= 99.5)
+            {
+                isTemperatureNormal = true;
+            }
+        }
+
+        if (hariDemam < config.GetBatasHariDemam())
+        {
+            isFeverDaysSafe = true;
+        }
+
+        Console.WriteLine("\nHasil Pemeriksaan:");
+        if (isTemperatureNormal && !isFeverDaysSafe)
+        {
+            Console.WriteLine(config.GetPesanDitolak());
+        }
+        else if (!isTemperatureNormal)
+        {
+            Console.WriteLine(config.GetPesanDitolak());
+        }
+        else
+        {
+            Console.WriteLine(config.GetPesanDiterima());
+        }
+
+        Console.WriteLine("\nMengubah satuan suhu...");
+        config.UbahSatuan();
+        Console.WriteLine("Satuan suhu sekarang: " + config.GetSatuanSuhu());
+
+        Console.WriteLine("\nTekan tombol apa saja untuk keluar...");
+        Console.ReadKey();
+    }
+}
